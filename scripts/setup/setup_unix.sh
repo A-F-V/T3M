ab6d84591312fb5cc7ebdde09496b708b712d732
A-F-V <faracea2000@gmail.com>
2020-06-12 17:02:53 +0100

    notebook created

--- scripts/setup/setup_unix.sh
+++ scripts/setup/setup_unix.sh
@@ -2,4 +2,5 @@
 [[ ! -d "env" ]] && python3 -m venv env
 source env/bin/activate
 pip3 install -r scripts/setup/packages.txt
+ipython kernel install --user --name=env
 deactivate
--- scripts/setup/setup_windows.bat
+++ scripts/setup/setup_windows.bat
@@ -2,4 +2,5 @@
 IF NOT EXIST env (python3 -m venv env)
 call env\Scripts\activate.bat
 pip3 install -r scripts/setup/packages.txt
+ipython kernel install --user --name=env
 deactivate
