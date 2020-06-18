206d4c26b571d3354459a18298c9bc35283c5a96
A-F-V <faracea2000@gmail.com>
2020-06-18 13:25:49 +0100

    Fixed shell script on linux and gitignore

--- scripts/setup/setup_unix.sh
+++ scripts/setup/setup_unix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+shopt -s expand_aliases
 command -v python >/dev/null 2>&1 || { alias python=python3; }
 command -v pip >/dev/null 2>&1 || { alias pip=pip3; }
 [[ ! -d "env" ]] && python -m venv env
