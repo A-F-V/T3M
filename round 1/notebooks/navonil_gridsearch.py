adc9baf742f5a0dcc14128f6ad5c8ea91f0597a1
NavonilNeogi <navonil.neogi@gmail.com>
2020-07-13 00:50:41 +0100

    Logistic Regression gridsearch

--- round 1/notebooks/navonil_gridsearch.py
+++
@@ -1,4 +0,0 @@
-optimisation = GridSearchCV()
-parameters = [{'solver':  [‘newton-cg’, ‘lbfgs’, ‘liblinear’, ‘sag’, ‘saga’], 'C': [1,2,3,300,500], 'penalty': [‘l1’, ‘l2’, ‘elasticnet’, ‘none’]}]
-search_model = optimisation(LogisticRegression(), parameters, scoring = 'accuracy')
-search_model.fit(training_set_X_trans, training_set_Y)
