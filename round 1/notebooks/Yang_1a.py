85ad598cc0c76b3d75ebec98ea24d59f18cb7d9f
A-F-V <faracea2000@gmail.com>
2020-07-17 15:06:12 +0100

    Fixed path in 'Yang_1a.py'

--- round 1/notebooks/Yang_1a.py
+++ round 1/notebooks/Yang_1a.py
@@ -1,44 +1,40 @@
+from sklearn import set_config
+from pathlib import Path
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.neural_network import MLPClassifier
+from sklearn import svm
+from sklearn.linear_model import RidgeClassifier, Perceptron
+from sklearn.ensemble import RandomForestClassifier
 'Main problem: I removed empty data rows instead of estimating them, hence I only have c.330 predictions instead of the desired 400+'
 
 
-import os,sys
+import os
+import sys
 
-globalT3Mbin = os.path.abspath(os.path.join(os.getcwd(),'..','..','bin'))
-round1bin = os.path.abspath(os.path.join(os.getcwd(),'..','bin'))
+print(os.getcwd())
+globalT3Mbin = os.path.abspath(os.path.join(os.getcwd(), '..', '..', 'bin'))
+round1bin = os.path.abspath(os.path.join(os.getcwd(), '..', 'bin'))
 if not (globalT3Mbin in sys.path):
     sys.path.append(globalT3Mbin)
 if not (round1bin in sys.path):
     sys.path.append(round1bin)
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import RidgeClassifier, Perceptron
-from sklearn import svm
-from sklearn.neural_network import MLPClassifier
-
-
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
-
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
 #from utilities import splitter
 
 
 'The below path would obviously only work on my computer, so running this code unedited probably isnt a good idea'
-from pathlib import Path
-data_folder_path = Path("C:/Users/yangyang/Documents/GitHub/T3M/round 1/data")
+data_folder_path = os.path.join(os.getcwd(), 'round 1', 'data')
 training_set_path = data_folder_path / "train.csv"
-
-
 
 
 seed = 1
 plt.style.use('dark_background')
-from sklearn import set_config
-#set_config(display='diagram')
+# set_config(display='diagram')
 
 
 '''
@@ -86,38 +82,38 @@ train_x, train_y = data_set[x_names], data_set['Survived']  #splits dataset into
 #train_y = train_y.dropna(axis=0)
 '''
 
-#randomforest
+# randomforest
 
-round1a_rf = RandomForestClassifier(random_state = 1)
-round1a_rf.fit(training_set_X_trans,training_set_Y)
+round1a_rf = RandomForestClassifier(random_state=1)
+round1a_rf.fit(training_set_X_trans, training_set_Y)
 a = round1a_rf.predict(test_set_X_trans)
 
-#ridgeclassification
+# ridgeclassification
 round1a_rc = RidgeClassifier(random_state=1)
-round1a_rc.fit(training_set_X_trans,training_set_Y)
+round1a_rc.fit(training_set_X_trans, training_set_Y)
 b = round1a_rc.predict(test_set_X_trans)
 
-#svm
+# svm
 round1a_svm = svm.SVC(random_state=1)
 round1a_svm.fit(training_set_X_trans, training_set_Y)
 c = round1a_svm.predict(test_set_X_trans)
 
 
-#MLPclassifier
+# MLPclassifier
 round1a_mlp = MLPClassifier(random_state=1)
 round1a_mlp.fit(training_set_X_trans, training_set_Y)
 d = round1a_mlp.predict(test_set_X_trans)
 
-#Perceptron
+# Perceptron
 round1a_perc = Perceptron(random_state=1)
 round1a_perc.fit(training_set_X_trans, training_set_Y)
 e = round1a_perc.predict(test_set_X_trans)
-#print(e)
+# print(e)
 
-#voting
+# voting
 vote = (((a+b+c+d+e)/5) > .5).astype(int)
 print(len(vote), '\n', type(vote))
-votedf = pd.DataFrame(data=vote, columns = ['Survived'])
+votedf = pd.DataFrame(data=vote, columns=['Survived'])
 votedf.to_excel('predictions.xlsx')
 
 
